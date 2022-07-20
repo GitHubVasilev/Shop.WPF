6372ec2142c9609a2b1e5fad57c1a9eda197361b
Антон Васильев <antohavas@gmail.com>
2022-07-20 21:54:10 +0300

    Create CustomerService

--- BusinessLogicLayer/Services/CustomersService.cs
+++ BusinessLogicLayer/Services/CustomersService.cs
@@ -3,46 +3,43 @@ using BusinessLogicLayer.DataTransferObject.Entitys;
 using BusinessLogicLayer.Interfaces;
 using DataAccessLayer.Entities;
 using DataAccessLayer.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BusinessLogicLayer.Services
 {
     public class CustomersService : IService<CustomerDTO>
     {
         private IRepository<Customer> _repository;
+        private IMapper _mapper;
 
-        public CustomersService(IUnitOfWork uof)
+        public CustomersService(IUnitOfWork uof, IMapperFactory mapperFactory)
         {
             _repository = uof.Customers;
+            _mapper = mapperFactory.CustomerMapper;
         }
 
         public void Create(CustomerDTO entity)
         {
-            _repository.Insert(entity)
+            _repository.Insert(_mapper.Map<Customer>(entity));
         }
 
         public void Delete(CustomerDTO entity)
         {
-            throw new NotImplementedException();
+            _repository.Delete(_mapper.Map<Customer>(entity));
         }
 
         public List<CustomerDTO> Get()
         {
-            throw new NotImplementedException();
+            return new List<CustomerDTO>(_mapper.Map<List<CustomerDTO>>(_repository.Get()));
         }
 
         public List<CustomerDTO> Get(string email)
         {
-            throw new NotImplementedException();
+            return new List<CustomerDTO>(_mapper.Map<List<CustomerDTO>>(_repository.Get(m=>m.Email == email)));
         }
 
         public void Update(CustomerDTO entity)
         {
-            throw new NotImplementedException();
+            _repository.Update(_mapper.Map<Customer>(entity));
         }
     }
 }
